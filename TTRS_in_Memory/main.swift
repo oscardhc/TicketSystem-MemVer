9fb3a3cba018b7d32c78506c5f5208c1257e1bf7
oscardhc <oscardhc@sjtu.edu.cn>
2020-03-18 10:49:13 +0800

    Initial Commit

---
+++ TTRS_in_Memory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TTRS_in_Memory
+//
+//  Created by Haichen Dong on 2020/3/18.
+//  Copyright © 2020 Haichen Dong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
